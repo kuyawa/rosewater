cf9d829d86d3294f641d6e49bbfa9cbff9d9f0f2
Kuyawa <kuyawakata@gmail.com>
2023-12-05 19:54:10 -0400

    donations

--- app/api/dreams/route.tsx
+++ app/api/dreams/route.tsx
@@ -62,6 +62,19 @@ export async function GET(request: NextRequest) {
   return Response.json(list)
 }
 
+interface File extends Blob {
+  readonly lastModified: number;
+  /** @deprecated */
+  readonly lastModifiedDate: Date;
+  readonly name: string;
+  readonly webkitRelativePath: string;
+}
+
+declare var File: {
+  prototype: File;
+  new (parts: (ArrayBuffer | ArrayBufferView | Blob | string)[], filename: string, properties?: FilePropertyBag): File;
+}
+
 export async function POST(request: NextRequest) {
   try {
     console.log('FORM')
